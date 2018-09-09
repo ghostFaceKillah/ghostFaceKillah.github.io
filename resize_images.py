b11c5d65a304950a94a01995284c70472737d972
ghostFaceKillah <garmulewicz@gmail.com>
2018-09-09 18:29:07 +0200

    Doing stuff, really

--- resize_images.py
+++ resize_images.py
@@ -12,12 +12,14 @@ def resize(fname, resized_fname, wanted_width=500):
 
 
 if __name__ == "__main__":
-    for fname in glob.glob('img/car/*/*.jpg') + glob.glob('img/car/*.jpg'):
+    wat = 'expert'
+    # wat = 'car'
+    for fname in glob.glob('img/{}/*/*.png'.format(wat)) + glob.glob('img/{}/*.png'.format(wat)):
         if fname.split('.')[0].split('_')[-1] != 'small':
             msg = "messing stuff up with bad '.' split for fname {}".format(fname)
             assert len(fname.split('.')) == 2, msg
             small_fname = fname.split('.')[0] + '_small.' + fname.split('.')[1]
 
-            print "Resizing {}".format(fname)
+            print("Resizing {}".format(fname))
             resize(fname, small_fname)
 
